758f7099334d0de515a55cb0e8960fe15425fb2b
ivan <ivany9@gmail.com>
2021-10-15 11:02:14 +1100

    carousel

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Portfolio from './pages/portfolio';
 import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="page-contanier">
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,18 +1,42 @@
 import React from 'react';
+    import {
+      MDBCarousel,
+      MDBCarouselInner,
+      MDBCarouselItem,
+      MDBCarouselElement,
+      MDBCarouselCaption,
+    } from 'mdb-react-ui-kit';
+
 
 const Portfolio = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Portfolio</h1>
-    </div>
-  );
-};
-
+        <MDBCarousel showIndicators showControls fade>
+          <MDBCarouselInner>
+            <MDBCarouselItem itemId={0}>
+              <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg' alt='...' />
+              <MDBCarouselCaption>
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </MDBCarouselCaption>
+            </MDBCarouselItem>
+    
+            <MDBCarouselItem itemId={1}>
+              <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg' alt='...' />
+              <MDBCarouselCaption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </MDBCarouselCaption>
+            </MDBCarouselItem>
+    
+            <MDBCarouselItem itemId={2}>
+              <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg' alt='...' />
+              <MDBCarouselCaption>
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </MDBCarouselCaption>
+            </MDBCarouselItem>
+          </MDBCarouselInner>
+        </MDBCarousel>
+      );
+    }
 export default Portfolio;
