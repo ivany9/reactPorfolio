2380c7cfffe4f46878341955d30ccea68ec19a2a
ivan <ivany9@gmail.com>
2021-10-16 19:20:16 +1100

    Input v

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,58 @@
 import { contains } from 'dom-helpers';
 import React from 'react';
 import {Form,Cont} from './pagesStyles';
+import React, { useState } from 'react';
+import {validateEmail, validatename } from '../utils/helpers'
 
 const Contact = () => {
+
+   const [email,setEmail]=useState('');
+   const [userName, setUserName]=useState('');
+   const [text,setText]=useState('');
+   const [errorMessage,setErrorMessage]=useState('');
+
+
+   const handleInputChange=(e) =>{
+   
+   const{target}=e;
+   const inputType=target.name;
+   const inputValue=target.value;
+   }
+
+   if(inputType==="name"){
+     setUserName(inputValue);
+   } else if (inputType==="email") {
+    setUserEmail(inputValue);
+   }else {
+        setText(inputValue);
+   }
+
+   const handleFormSubmit = (e) =>{
+
+    e.preventDefault();
+   
+       if(!validatename(userName)){
+         setErrorMessage('Name no valid');
+         return;
+         }
+    
+       if(!validateEmail(email)){
+          
+         setErrorMessage('Email no valid');
+         }
+
+         setUserName('');
+         setEmail('');
+
+
+
+   }
+
+
+
+
+
+
   return (
     
     <Cont>
@@ -10,17 +60,17 @@ const Contact = () => {
         Contact
     <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input name="name"type="text" className="form-control" placeholder="Jhon Smith"/>
+        <input value={userName} onChange={handleInputChange}  type="text" className="form-control" placeholder="Jhon Smith"/>
     </div>
     <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email address</label>
-        <input name="email" type="email" className="form-control" aria-describedby="emailHelp" placeholder="name@example.com" />
+        <input  value={email} onChange={handleInputChange}  name="email" type="email" className="form-control" aria-describedby="emailHelp" placeholder="name@example.com" />
     </div>
     <div className="form-group">
         <label htmlFor="message">Message</label>
-        <textarea className="form-control" rows="10"></textarea>
+        <textarea value={text} className="form-control" rows="10"></textarea>
     </div>
-    <button type="submit" className="btn btn-danger ">Submit</button>
+    <button type="submit" className="btn btn-danger " onClick={handleFormSubmit} >Submit</button>
 </Form>
 </Cont> 
 
